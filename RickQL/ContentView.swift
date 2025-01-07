afae76512e9660aae1ce0391983c182b83166b1c
vcosmin2701 <v.cosmin2717@gmail.com>
2025-01-07 16:11:30 +0200

    Initial Commit

---
+++ RickQL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickQL
+//
+//  Created by Vladut Cosmin on 07.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickQL/RickQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickQLApp.swift
+//  RickQL
+//
+//  Created by Vladut Cosmin on 07.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct RickQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
